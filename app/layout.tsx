aba2ee8fe3ed9482175b364576128488a8b0ea42
Gabriel Alves <gabriel_vieira2018@hotmail.com>
2024-01-23 12:19:17 +0100

    Add Next UI and Framer Motion dependencies, update layout, and configured Next UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Gabriel Alves - Junior Java Developer",
@@ -12,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
 
+const { nextui } = require("@nextui-org/react");
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,6 +21,36 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar"), ("tailwindcss-textshadow")],
+  darkMode: "class",
+  plugins: [
+    require("tailwind-scrollbar"),
+    "tailwindcss-textshadow",
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            // background: "rgb(165, 243, 252)",
+            // foreground: "rgb(251, 207, 232)",
+            primary: {
+              // DEFAULT: "#11D1BE",
+              // foreground: "#000000",
+            },
+            // focus: "#11D1BE",
+          },
+        },
+        dark: {
+          colors: {
+            // background: "rgb(3, 105, 161)",
+            // foreground: "rgb(251, 207, 232)",
+            primary: {
+              // DEFAULT: "#11D1BE",
+              // foreground: "#000000",
+            },
+            // focus: "#11D1BE",
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
