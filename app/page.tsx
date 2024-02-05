d2ef6b9649fb2ffda5182162412517ec8537d842
Gabriel Alves <gabriel_vieira2018@hotmail.com>
2024-02-05 15:17:59 +0100

    Update font size and image dimensions for mobile in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <Container>
         <div className="h-screen flex flex-col justify-evenly items-center sm:flex-row">
-          <div className="text-4xl text-center sm:text-left font-bold sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl 2xl:text-6xl">
+          <div className="text-3xl text-center sm:text-left font-bold sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl 2xl:text-6xl">
             <p>HI, I&#39;M GABRIEL.</p>
             <p>A CREATIVE</p>
             <p>AND DEDICATED</p>
@@ -20,7 +20,7 @@ export default function Home() {
             width={450}
             height={450}
             priority
-            className="rounded-full w-[350px] h-[350px] sm:w-[210px] sm:h-[210px] md:w-[250px] md:h-[250px] lg:w-[320px] lg:h-[320px] xl:w-[350px] xl:h-[350px] 2xl:w-[450px] 2xl:h-[450px]"
+            className="rounded-full w-[300px] h-[300px] sm:w-[210px] sm:h-[210px] md:w-[250px] md:h-[250px] lg:w-[320px] lg:h-[320px] xl:w-[350px] xl:h-[350px] 2xl:w-[450px] 2xl:h-[450px]"
           />
         </div>
       </Container>
