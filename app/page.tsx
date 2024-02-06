7ba2fe844cc8e66265c3d937be5712afc992ca97
Gabriel Alves <gabriel_vieira2018@hotmail.com>
2024-02-06 12:11:57 +0100

    Refactor home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,24 @@ export default function Home() {
   return (
     <main>
       <Container>
-        <div className="home-container">
-          <div className="home-elements-container">
-            <div className="home-text-container">
-              <p>HI, I&#39;M GABRIEL.</p>
-              <p>A CREATIVE</p>
-              <p>AND DEDICATED</p>
-              <p>FRONTEND DEVELOPER</p>
-              <p>AIMING TO BE A</p>
-              <p>FULL STACK DEVELOPER</p>
-            </div>
+        <div className="home-outer-container">
+          <div className="home-text-container">
+            <p>HI, I&#39;M GABRIEL.</p>
+            <p>A CREATIVE</p>
+            <p>AND DEDICATED</p>
+            <p>FRONTEND DEVELOPER</p>
+            <p>AIMING TO BE A</p>
+            <p>FULL STACK DEVELOPER</p>
+          </div>
+          <div className="home-inner-container">
+            <Image
+              src={"/images/photo-bg-shadow.jpg"}
+              alt="profile photo"
+              width={450}
+              height={450}
+              priority
+              className="home-profile-img"
+            />
             <div className="home-icon-btn-container">
               <button className="home-icon-btn group">
                 <a href="cv/Gabriel_Alves_CV_EN.pdf" download>
@@ -37,14 +45,6 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <Image
-            src={"/images/photo-bg-shadow.jpg"}
-            alt="profile photo"
-            width={450}
-            height={450}
-            priority
-            className="home-profile-img"
-          />
         </div>
       </Container>
     </main>
